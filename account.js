6217c1848606fcf2232ca48836e6e7a3be4b42af
lucaspouchot <lucas.pouchot@gmail.com>
2019-03-07 18:11:50 +0100

    change colors + start implement post api

--- account.js
+++ account.js
@@ -8,6 +8,7 @@ ejs.renderFile(
   { async: false },
   (_err, _str) => {
     document.getElementById('wizard_account').innerHTML = _str
+
     form_params.account = {}
     form_params.account.useAlreadyExist = false
     form_params.account.login = ''
@@ -18,153 +19,175 @@ ejs.renderFile(
     form_params.account.pwdConfirmation = ''
     form_params.account._willShowErrors = null
 
-    params.views[_wizard_view_account_index].isOk = function(willShowErrors){
-      _errors = []
-
-      form_params.account.login = form_params.account.login.trim()
-      form_params.account.mail = form_params.account.mail.trim()
-      form_params.account.firstname = form_params.account.firstname.trim()
-      form_params.account.lastname = form_params.account.lastname.trim()
-
-      if(form_params.account.login === ''){
-        _errors[_errors.length] = { title: 'Login', corpus: 'your login is empty'}
-      }
-
-      if(form_params.account.pwd === ''){
-        _errors[_errors.length] = { title: 'Password', corpus: 'password is empty'}
-      }
-
-      if(form_params.account.useAlreadyExist === false){
-        if(form_params.account.mail === ''){
-          _errors[_errors.length] = { title: 'Email', corpus: 'your email is empty'}
-        } else if(!validateEmail(form_params.account.mail)){
-          _errors[_errors.length] = { title: 'Email', corpus: 'your email is not valid'}
-        }
-
-        if(form_params.account.firstname === ''){
-          _errors[_errors.length] = { title: 'Firstname', corpus: 'firstname is empty'}
-        }
-        if(form_params.account.lastname === ''){
-          _errors[_errors.length] = { title: 'Lastname', corpus: 'lastname is empty'}
-        }
-
-        if(form_params.account.pwd !== form_params.account.pwdConfirmation){
-          _errors[_errors.length] = { title: 'Password confirmation', corpus: 'there is a diff between your password and your password confirmation'}
-        }
-      }
-
-      if(willShowErrors === true || _errors.length === 0){
-        showErrors(_errors, params.views[_wizard_view_account_index].order)
-      }
-
-      if(_errors.length === 0){
-        return true
-      }
-      return false
-    }
-
-    params.views[_wizard_view_account_index].getResumed = function(){
-      var _html = ''
-      if(form_params.account.ignore){
-        _html =  `The account settings will be ignored`
-      } else {
-        if(form_params.account.useAlreadyExist){
-          _html =  `You will use the existing account for <b>${form_params.account.login}</b>.`
-        } else {
-          var _liStyle = 'background-color: rgba(0, 0, 0, 0); border-bottom-width: 1px;'
-          _html =  `
-      <ul class="collection" style="border-width:0px">
-        <li style="${_liStyle}" class="collection-header"><b>Login</b><br/>${form_params.account.login || 'not set'}</li>
-        <li style="${_liStyle}" class="collection-header"><b>Password</b><br/>${form_params.account.pwd.replace(/./g, "&#183;") || 'not set'}</li>
-        <li style="${_liStyle}" class="collection-header"><b>Email</b><br/>${form_params.account.mail || 'not set'}</li>
-        <li style="${_liStyle}" class="collection-header"><b>Firstname</b><br/>${form_params.account.firstname || 'not set'}</li>
-        <li style="${_liStyle} border-bottom-width: 0px;" class="collection-header"><b>Lastname</b><br/>${form_params.account.lastname || 'not set'}</li>
-      </ul>`
-        }
-      }
-      return _html
-    }
-
     document.getElementById("wizard_account_form_exist").addEventListener('input', accountExistChange);
-    function accountExistChange(e){
-      form_params.account.useAlreadyExist = document.getElementById('wizard_account_form_exist').checked
-      var _account_hide_on_already_exist = document.getElementsByClassName("account_hide_on_already_exist");
-      if(_account_hide_on_already_exist){
-        if(form_params.account.useAlreadyExist){
-          for(var i=0, max=_account_hide_on_already_exist.length; i<max; i++){
-            _account_hide_on_already_exist[i].classList.remove('is-visible')
-          }
-        } else {
-          for(var i=0, max=_account_hide_on_already_exist.length; i<max; i++){
-            _account_hide_on_already_exist[i].classList.add('is-visible')
-          }
-        }
-      }
-      params.views[_wizard_view_account_index].checkButtonNextStats()
-      if(form_params.account._willShowErrors){
-        clearTimeout(form_params.account._willShowErrors)
-      }
-      form_params.account._willShowErrors = setTimeout(() => { params.views[_wizard_view_account_index].isOk(true) }, 3000)
-    }
-
     document.getElementById("wizard_account_form_login").addEventListener('input', accountLoginChange);
-    function accountLoginChange(e){
-      form_params.account.login = document.getElementById('wizard_account_form_login').value
-      params.views[_wizard_view_account_index].checkButtonNextStats()
-      if(form_params.account._willShowErrors){
-        clearTimeout(form_params.account._willShowErrors)
-      }
-      form_params.account._willShowErrors = setTimeout(() => { params.views[_wizard_view_account_index].isOk(true) }, 3000)
-    }
-
     document.getElementById("wizard_account_form_mail").addEventListener('input', accountMailChange);
-    function accountMailChange(e){
-      form_params.account.mail = document.getElementById('wizard_account_form_mail').value
-      params.views[_wizard_view_account_index].checkButtonNextStats()
-      if(form_params.account._willShowErrors){
-        clearTimeout(form_params.account._willShowErrors)
-      }
-      form_params.account._willShowErrors = setTimeout(() => { params.views[_wizard_view_account_index].isOk(true) }, 3000)
-    }
-
     document.getElementById("wizard_account_form_firstname").addEventListener('input', accountFirstnameChange);
-    function accountFirstnameChange(e){
-      form_params.account.firstname = document.getElementById('wizard_account_form_firstname').value
-      params.views[_wizard_view_account_index].checkButtonNextStats()
-      if(form_params.account._willShowErrors){
-        clearTimeout(form_params.account._willShowErrors)
-      }
-      form_params.account._willShowErrors = setTimeout(() => { params.views[_wizard_view_account_index].isOk(true) }, 3000)
-    }
-
     document.getElementById("wizard_account_form_lastname").addEventListener('input', accountLastnameChange);
-    function accountLastnameChange(e){
-      form_params.account.lastname = document.getElementById('wizard_account_form_lastname').value
-      params.views[_wizard_view_account_index].checkButtonNextStats()
-      if(form_params.account._willShowErrors){
-        clearTimeout(form_params.account._willShowErrors)
-      }
-      form_params.account._willShowErrors = setTimeout(() => { params.views[_wizard_view_account_index].isOk(true) }, 3000)
-    }
-
     document.getElementById("wizard_account_form_psswrd").addEventListener('input', accountPasswordChange);
-    function accountPasswordChange(e){
-      form_params.account.pwd = document.getElementById('wizard_account_form_psswrd').value
-      params.views[_wizard_view_account_index].checkButtonNextStats()
-      if(form_params.account._willShowErrors){
-        clearTimeout(form_params.account._willShowErrors)
-      }
-      form_params.account._willShowErrors = setTimeout(() => { params.views[_wizard_view_account_index].isOk(true) }, 3000)
-    }
-
     document.getElementById("wizard_account_form_psswrd_confirmtn").addEventListener('input', accountPasswordConfirmationChange);
-    function accountPasswordConfirmationChange(e){
-      form_params.account.pwdConfirmation = document.getElementById('wizard_account_form_psswrd_confirmtn').value
-      params.views[_wizard_view_account_index].checkButtonNextStats()
-      if(form_params.account._willShowErrors){
-        clearTimeout(form_params.account._willShowErrors)
-      }
-      form_params.account._willShowErrors = setTimeout(() => { params.views[_wizard_view_account_index].isOk(true) }, 3000)
-    }
+
   }
 )
+
+params.views[_wizard_view_account_index].post = function(){
+  var request = new Request(params.views[_wizard_view_account_index].api)
+
+  var ok = params.views[_wizard_view_account_index].isOk()
+  if(!ok){
+    request.setData({})
+  } else {
+    request.setData({
+      login: form_params.account.login,
+      email: form_params.account.mail,
+      password: CryptoJS.SHA512(form_params.account.pwd).toString().toUpperCase(),
+      firstname: form_params.account.firstname,
+      lastname: form_params.account.lastname,
+      useAlreadyExist: form_params.account.useAlreadyExist,
+      deviceName: requestAlive.isLocalUrl()?params.hostname:requestAlive.getHost()
+    })
+  }
+  return request.post()
+}
+
+params.views[_wizard_view_account_index].isOk = function(willShowErrors){
+  _errors = []
+
+  form_params.account.login = form_params.account.login.trim()
+  form_params.account.mail = form_params.account.mail.trim()
+  form_params.account.firstname = form_params.account.firstname.trim()
+  form_params.account.lastname = form_params.account.lastname.trim()
+
+  if(form_params.account.login === ''){
+    _errors[_errors.length] = { title: 'Login', corpus: 'your login is empty'}
+  }
+
+  if(form_params.account.pwd === ''){
+    _errors[_errors.length] = { title: 'Password', corpus: 'password is empty'}
+  }
+
+  if(form_params.account.useAlreadyExist === false){
+    if(form_params.account.mail === ''){
+      _errors[_errors.length] = { title: 'Email', corpus: 'your email is empty'}
+    } else if(!validateEmail(form_params.account.mail)){
+      _errors[_errors.length] = { title: 'Email', corpus: 'your email is not valid'}
+    }
+
+    if(form_params.account.firstname === ''){
+      _errors[_errors.length] = { title: 'Firstname', corpus: 'firstname is empty'}
+    }
+    if(form_params.account.lastname === ''){
+      _errors[_errors.length] = { title: 'Lastname', corpus: 'lastname is empty'}
+    }
+
+    if(form_params.account.pwd !== form_params.account.pwdConfirmation){
+      _errors[_errors.length] = { title: 'Password confirmation', corpus: 'there is a diff between your password and your password confirmation'}
+    }
+  }
+
+  if(willShowErrors === true || _errors.length === 0){
+    showErrors(_errors, params.views[_wizard_view_account_index].order)
+  }
+
+  if(_errors.length === 0){
+    return true
+  }
+  return false
+}
+
+params.views[_wizard_view_account_index].getResumed = function(){
+  var _html = ''
+  if(form_params.account.ignore){
+    _html =  `The account settings will be ignored`
+  } else {
+    if(form_params.account.useAlreadyExist){
+      _html =  `You will use the existing account for <b>${form_params.account.login}</b>.`
+    } else {
+      var _liStyle = 'background-color: rgba(0, 0, 0, 0); border-bottom-width: 1px;'
+      _html =  `
+  <ul class="collection" style="border-width:0px">
+    <li style="${_liStyle}" class="collection-header"><b>Login</b><br/>${form_params.account.login || 'not set'}</li>
+    <li style="${_liStyle}" class="collection-header"><b>Password</b><br/>${form_params.account.pwd.replace(/./g, "&#183;") || 'not set'}</li>
+    <li style="${_liStyle}" class="collection-header"><b>Email</b><br/>${form_params.account.mail || 'not set'}</li>
+    <li style="${_liStyle}" class="collection-header"><b>Firstname</b><br/>${form_params.account.firstname || 'not set'}</li>
+    <li style="${_liStyle} border-bottom-width: 0px;" class="collection-header"><b>Lastname</b><br/>${form_params.account.lastname || 'not set'}</li>
+  </ul>`
+    }
+  }
+  return _html
+}
+
+function accountExistChange(e){
+  form_params.account.useAlreadyExist = document.getElementById('wizard_account_form_exist').checked
+  var _account_hide_on_already_exist = document.getElementsByClassName("account_hide_on_already_exist");
+  if(_account_hide_on_already_exist){
+    if(form_params.account.useAlreadyExist){
+      for(var i=0, max=_account_hide_on_already_exist.length; i<max; i++){
+        _account_hide_on_already_exist[i].classList.remove('is-visible')
+      }
+    } else {
+      for(var i=0, max=_account_hide_on_already_exist.length; i<max; i++){
+        _account_hide_on_already_exist[i].classList.add('is-visible')
+      }
+    }
+  }
+  params.views[_wizard_view_account_index].checkButtonNextStats()
+  if(form_params.account._willShowErrors){
+    clearTimeout(form_params.account._willShowErrors)
+  }
+  form_params.account._willShowErrors = setTimeout(() => { params.views[_wizard_view_account_index].isOk(true) }, 3000)
+}
+
+function accountLoginChange(e){
+  form_params.account.login = document.getElementById('wizard_account_form_login').value
+  params.views[_wizard_view_account_index].checkButtonNextStats()
+  if(form_params.account._willShowErrors){
+    clearTimeout(form_params.account._willShowErrors)
+  }
+  form_params.account._willShowErrors = setTimeout(() => { params.views[_wizard_view_account_index].isOk(true) }, 3000)
+}
+
+function accountMailChange(e){
+  form_params.account.mail = document.getElementById('wizard_account_form_mail').value
+  params.views[_wizard_view_account_index].checkButtonNextStats()
+  if(form_params.account._willShowErrors){
+    clearTimeout(form_params.account._willShowErrors)
+  }
+  form_params.account._willShowErrors = setTimeout(() => { params.views[_wizard_view_account_index].isOk(true) }, 3000)
+}
+
+function accountFirstnameChange(e){
+  form_params.account.firstname = document.getElementById('wizard_account_form_firstname').value
+  params.views[_wizard_view_account_index].checkButtonNextStats()
+  if(form_params.account._willShowErrors){
+    clearTimeout(form_params.account._willShowErrors)
+  }
+  form_params.account._willShowErrors = setTimeout(() => { params.views[_wizard_view_account_index].isOk(true) }, 3000)
+}
+
+function accountLastnameChange(e){
+  form_params.account.lastname = document.getElementById('wizard_account_form_lastname').value
+  params.views[_wizard_view_account_index].checkButtonNextStats()
+  if(form_params.account._willShowErrors){
+    clearTimeout(form_params.account._willShowErrors)
+  }
+  form_params.account._willShowErrors = setTimeout(() => { params.views[_wizard_view_account_index].isOk(true) }, 3000)
+}
+
+function accountPasswordChange(e){
+  form_params.account.pwd = document.getElementById('wizard_account_form_psswrd').value
+  params.views[_wizard_view_account_index].checkButtonNextStats()
+  if(form_params.account._willShowErrors){
+    clearTimeout(form_params.account._willShowErrors)
+  }
+  form_params.account._willShowErrors = setTimeout(() => { params.views[_wizard_view_account_index].isOk(true) }, 3000)
+}
+
+function accountPasswordConfirmationChange(e){
+  form_params.account.pwdConfirmation = document.getElementById('wizard_account_form_psswrd_confirmtn').value
+  params.views[_wizard_view_account_index].checkButtonNextStats()
+  if(form_params.account._willShowErrors){
+    clearTimeout(form_params.account._willShowErrors)
+  }
+  form_params.account._willShowErrors = setTimeout(() => { params.views[_wizard_view_account_index].isOk(true) }, 3000)
+}
--- api.js
+++ api.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser');
 const path = require('path')
 const fs = require('fs')
+const InterfaceUtils = require('ttbd-interface-utils')
+const interface_utils = new InterfaceUtils()
 
 const name = 'account'
 var settingsPath = null
@@ -9,8 +11,16 @@ var stats = {
   initialized: false,
   status: 'nok',
   validateAction: 'none',
-  linked: false,
-  mail: null
+  coldfacts: {
+    linked: false,
+    mail: '',
+    account: ''
+  },
+  zoib: {
+    linked: false,
+    login: '',
+    cipheredPassword: ''
+  }
 }
 
 function init(app, apiToExpose, persistenceDir) {
@@ -21,15 +31,50 @@ function init(app, apiToExpose, persistenceDir) {
   settingsPath = path.join(settingsPath, 'settings.json')
   syncStats()
 
-  app.use(apiToExpose, bodyParser.urlencoded({ extended: true }));
+  app.use(apiToExpose, bodyParser.json());
   app.get(apiToExpose, function(req, res){
     syncStats()
     res.json(stats)
   });
 
   app.post(apiToExpose, function(req, res){
-    syncStats()
-    res.json(stats)
+    var data = req.body;
+    var allGood = true
+
+    if(!data.login || data.login === '' || !data.password || data.password === '' || !data.deviceName || data.deviceName === ''){
+      allGood = false
+    } else if(data.useAlreadyExist === 'false' || data.useAlreadyExist === false){
+      if(!data.email || data.email === '' || !data.firstname || data.firstname === '' || !data.lastname || data.lastname === ''){
+        allGood = false
+      }
+    }
+
+    if(allGood){
+      interface_utils.ZOIB.setAccount(data.login, data.password)
+      interface_utils.ZOIB.login()
+      .then(status => {
+        if(status.message.toUpperCase() === 'OK'){
+          return interface_utils.localCipher(data.password)
+        } else {
+          throw new Error(status.message)
+        }
+      })
+      .then( cipheredPassword => {
+        res.json(stats)
+      })
+      .catch( err => {
+        var message = err
+        if(err instanceof Error){
+          message = err.message
+        } else if(typeof err.toString === 'function'){
+          message = err.toString()
+        }
+        res.status(500).json({ message: message })
+      })
+
+    } else {
+      res.status(403).json({ message: "wrong params" })
+    }
   });
 }
 
